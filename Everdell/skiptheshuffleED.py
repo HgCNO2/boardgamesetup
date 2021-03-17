ce6b2a28192a82154bfc4df0b3fab07e7bc0fb0b
Eric Heiken <48847746+HgCNO2@users.noreply.github.com>
2021-03-16 22:26:16 -0400

    Replaced while loops with random sample
    Replace all while loops with random.sample to build final output lists.

--- Everdell/skiptheshuffleED.py
+++ Everdell/skiptheshuffleED.py
@@ -17,24 +17,13 @@ setupSpecEvent = []
 numPlayers = int(input('How many players are playing? 1, 2, 3, or 4?\n'))
 
 # Randomly Generate Forest Locations
-randForestIndex = random.randint(0, len(allForest)-1)
 if numPlayers <=2:
-    while len(setupForest) < 3:
-        setupForest.append(allForest[randForestIndex])
-        allForest.pop(randForestIndex)
-        randForestIndex = random.randint(0, len(allForest)-1)
+    setupForest = random.sample(allForest, k=3)
 else:
-    while len(setupForest) < 4:
-        setupForest.append(allForest[randForestIndex])
-        allForest.pop(randForestIndex)
-        randForestIndex = random.randint(0, len(allForest)-1)
+    setupForest = random.sample(allForest, k=4)
 print('\nYour forest locations are: \n--' + '\n--'.join(setupForest))
     
 # Randomly Generate Special Events
-randSpecEventIndex = random.randint(0, len(allSpecEvent)-1)
-while len(setupSpecEvent) < 4:
-    setupSpecEvent.append(allSpecEvent[randSpecEventIndex])
-    allSpecEvent.pop(randSpecEventIndex)
-    randSpecEventIndex = random.randint(0, len(allSpecEvent)-1)
+setupSpecEvent = random.sample(allSpecEvent, k=4)
 print('\nYour special events are: \n--' + '\n--'.join(setupSpecEvent))
 input('\nEnjoy the game!\nPress ENTER to exit.')
